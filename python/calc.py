c5fe00e99f436910a4019ef05da8125aa960a677
CountryFlower <awl18565151938@163.com>
2020-03-30 23:48:04 +0800

    修改作业

--- python/calc.py
+++ python/calc.py
@@ -1,17 +1,8 @@
 
 class Calc:
     def add(self,a,b):
-        try:
-            res = a + b
-        except Exception as e:
-            res = '参数类型错误'
-        return res
+        return a + b
 
     def div(self,a,b):
-        try:
-            res = a / b
-        except ZeroDivisionError:
-            res = '除数不能为0'
-        except Exception:
-            res = '参数类型错误'
-        return res
+        return a / b
+
--- testcase/test_calc.py
+++ testcase/test_calc.py
@@ -1,25 +1,72 @@
 from python.calc import Calc
 import pytest
 
+add_data = [
+    (1, 2, 3),
+    (2, -3, -1),
+    (3, -2, 1),
+    (2, -2, 0),
+    (-2, -1, -3),
+    (0.0003, 0.002, 0.0023),
+    (2, 0.01, 2.01),
+    (-1, 0.01, -0.99),
+    (999999, 888888, 1888887),
+    (0, 1, 1),
+    (0, -1, -1),
+    (0, 0, 0)
+]
+
+div_data = [
+    (2, 1, 2),
+    (-2, -1, 2),
+    (2, 3, 0.6666666666666666),
+    (1, 0.25, 4),
+    (0.25, 4, 0.0625),
+    (-5, 8, -0.625),
+    (0, 1, 0),
+    (1, 0, 1),
+    (0, 0, 1),
+    (9999999,1111111,9)
+]
 
 class TestCalc:
     def setup(self):
         self.calc = Calc()
 
-    def test_add_1(self):
-        assert self.calc.add(1, 2) == 3
+    '''
+       1.正整数相加
+       2.一正一负相加（结果为正，负，0）
+       3.负数相加
+       4.小数相加
+       5.整数加小数（正负）
+       6.大数相加
+       7. 0和整数 （正负）
+       8. 都为0
+    '''
+    @pytest.mark.parametrize("a,b,r",add_data)
+    def test_add_1(self,a,b,r):
+        assert self.calc.add(a, b) == r
 
-    def test_add_2(self):
-        assert self.calc.add('a',1) == '参数类型错误'
-
-    def test_div_1(self):
-        assert self.calc.div(1, 5) == 0.2
-
-    def test_div_2(self):
-        assert self.calc.div(1,0) == '除数不能为0'
-
-    def test_div_3(self):
-        assert self.calc.div(1,'a') == '参数类型错误'
+    '''
+        1.正整数相除
+        2.负整数相除
+        3.分子或分母为小数
+        4.结果为正整数
+        5.结果为小数
+        6.结果负数
+        7.分子为0
+        8.分母为0
+        9.两者都为0
+        10. 大数
+    '''
+    @pytest.mark.parametrize(['a','b','r'],div_data)
+    def test_div_1(self,a,b,r):
+        if b == 0:
+            with pytest.raises(ZeroDivisionError,match='除数不能为0'):
+                raise ZeroDivisionError('除数不能为0')
+        else:
+            assert self.calc.div(a, b) == r
 
 # if __name__ == '__main__':
 #     pytest.main()
+
