f2be632c50e90fb5f6670f69eebed7a61b084e79
CountryFlower <awl18565151938@163.com>
2020-04-02 22:10:24 +0800

    修改作业

--- web/test_work_wx.py
+++ web/test_work_wx.py
@@ -20,17 +20,28 @@ class TestWorkWX:
         # self.driver.quit()
 
     def test_add_mem(self):
-        self.driver.get('https://work.weixin.qq.com/wework_admin/loginpage_wx')
-        self.driver.implicitly_wait(5)
-        time.sleep(10)
+        time.sleep(5)
         #获取cookie
+        self.driver.get('https://work.weixin.qq.com/wework_admin/frame')
+        time.sleep(10)
+        self.driver.implicitly_wait(10)
+
         cookies:list(dict) = self.driver.get_cookies()
-        for cookie in cookies:
-            with open('work_wx.txt','a') as f:
-                json.dump(cookie,f)
-        wait = WebDriverWait(self.driver,10)
+        with open('work_wx.txt','w') as f:
+           json.dump(cookies,f)
+
+        with open('work_wx.txt','r') as f:
+            cookies = json.load(f)
+            for cookie in cookies:
+                if 'expiry' in cookie:
+                    cookie.pop('expiry')
+                self.driver.add_cookie(cookie)
+        self.driver.get('https://work.weixin.qq.com/wework_admin/frame')
+
+        wait = WebDriverWait(self.driver,30)
         # #index页的添加成员
         # wait.until(lambda driver: driver.find_element(By.XPATH,'//*[@class="index_service_cnt_item"]')).click()
         wait.until(lambda driver: driver.find_element(By.CSS_SELECTOR, '#menu_contacts')).click()
         # 通讯录页的添加成员
-        wait.until(lambda driver: driver.find_element(By.XPATH, '//*[@class="ww_operationBar"]//a[text()="添加成员"]')).click()
+        add_mem = self.driver.find_element(By.CSS_SELECTOR, '.ww_operationBar .js_add_member')
+        add_mem.click()
